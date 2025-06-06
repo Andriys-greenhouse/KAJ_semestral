0d8cf904ae6228e32d28eb302ed1b16b270f7009
Ondřej Samsounek <ondrej.samsounek@gmail.com>
2025-06-06 18:19:13 +0200

    Added init code Add init code to `main.ts` Add event listeners on "storage" event on window and click on "add-button". S.J.C.G.(=solo Jesus Christos gloria)

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
-import { onOnline, onOffline, cleanupLocalStorage } from "./utils";
+import { onOnline, onOffline, updateTimerList, updateDisplayedList, updateDisplayed, updateTimezoneLabel, updateTimeLabel, getTimerWithDefVals } from "./utils";
+import { getTimersCpy, setupLocalStorage, updateTimers } from "./lsManagement";
+
+function onStorage() {
+    updateTimerList();
+    updateDisplayedList();
+}
+
+let editModeOn = false;
+function editOn() {
+    editModeOn = true;
+    // TODO: make the rest of adjustments needed for putting the edit mode on
+}
+
+function editOff() {
+    editModeOn = false;
+    onStorage();
+    // TODO: make the rest of adjustments needed for putting the edit mode off
+}
 
 // find out whether online
 addEventListener("offline", onOffline);
@@ -7,4 +25,28 @@ addEventListener("online", onOnline);
 (navigator.onLine ? onOnline : onOffline)(new Event(""));
 
 // load timers
-cleanupLocalStorage();
+setupLocalStorage();
+updateTimerList();
+
+// on storage
+window.addEventListener("storage", onStorage);
+
+// add-button
+document.querySelector("#add-timer-button").addEventListener("click", (e) => {
+    const timers = getTimersCpy();
+    timers.push(getTimerWithDefVals());
+    updateTimers(timers);
+    onStorage();
+});
+
+// on animation frame:
+//   -- for each of displayed call `updateDisplayed()`
+//   -- set timezone lable ( `updateTimezoneLabel()` )
+//   -- update displayed time ( `updateTimeLabel()` )
+window.requestAnimationFrame(() => {
+    document.querySelector("#displayed-list").childNodes.forEach((nd) => {
+        updateDisplayed(nd as HTMLLabelElement);
+    });
+    updateTimezoneLabel();
+    updateTimeLabel();
+})
